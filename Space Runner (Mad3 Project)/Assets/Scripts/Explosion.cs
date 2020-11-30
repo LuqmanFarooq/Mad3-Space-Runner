3ae3006c43fac784f853a6eac3feef4b005a2d15
LuqmanFarooq <g00353385@gmit.ie>
2020-11-30 04:41:05 +0000

    Added player trail , follow player camera and explosion effect of player

---
+++ Space Runner (Mad3 Project)/Assets/Scripts/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+    
+    void IveBeenHit(Vector3 pos)
+    {
+        GameObject go = Instantiate(explosion, pos, Quaternion.identity, transform) as GameObject;
+        Destroy(go, 6f);
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+            IveBeenHit(contact.point);
+            Destroy(gameObject, 3);
+    }
+}
