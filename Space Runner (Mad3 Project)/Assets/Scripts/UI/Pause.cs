7c2cec388ae3a44823a487db2eef92cceca614ff
LuqmanFarooq <g00353385@gmit.ie>
2020-12-15 15:50:04 +0000

    completed game,developer diary,added test plan excel file,screenshot of buildsettings and screencast video of gameplay

--- Space Runner (Mad3 Project)/Assets/Scripts/UI/Pause.cs
+++ Space Runner (Mad3 Project)/Assets/Scripts/UI/Pause.cs
@@ -6,17 +6,36 @@ public class Pause : MonoBehaviour
 {
     public GameObject PausePanel;
     public GameObject gameUi;
-    
+    static bool GameIsPaused = false;
+    // pausing the game
     public void ClickPause()
     {
         PausePanel.SetActive(true);
         gameUi.SetActive(false);
         Time.timeScale = 0;
+        GameIsPaused = true;
     }
+    //resuming the game
     public void Resume()
     {
         PausePanel.SetActive(false);
         gameUi.SetActive(true);
         Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+    void Update()
+    {
+        // pause and resume with esc button
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                ClickPause();
+            }
+        }
     }
 }
